0f12d5afdb4cb044e4101a6a55f588f5aab14cff
古谷　優輝 <furuyayuuki@furuya.local>
2015-06-30 10:37:18 +0900

    完成

--- src/taxi.php
+++ src/taxi.php
@@ -8,4 +8,134 @@ namespace TripleI\taxi;
 
 class taxi
 {
+    /**
+     *入力データ
+     *@var str
+     */
+    public $inputdata;
+
+    /**
+     *初乗りデータ
+     *@var array
+     */
+    public $startdata;
+
+    /**
+     *区間
+     *@var array
+     */
+    public $intervalstr;
+
+    /**
+     *区間の距離と料金
+     *@var array
+     */
+    public $intervalint;
+
+    /**
+     *合計の料金
+     *@var int
+     */
+    public $pricecalc;
+
+    public function InputData($data)
+    {
+        return $this->inputdata = $data;
+    }
+
+    public function StartData()
+    {
+        $inputdata = $this->inputdata;
+
+        $startdata = array();
+        $firststr = mb_substr($inputdata, 0, 1);
+
+        switch($firststr) {
+            case "A":
+            case "B":
+            case "C":
+                array_push($startdata, 995, 400);
+                break;
+            case "D":
+            case "E":
+            case "F":
+            case "G":
+                array_push($startdata, 845, 350);
+                break;
+        }
+
+        return $this->startdata = $startdata;
+    }
+
+    public function IntervalStr()
+    {
+        $inputdata = $this->inputdata;
+
+        $intervalcount = strlen($inputdata)-1;
+        $intervalstr = array();
+        $trialcount = 0;
+
+        while ($intervalcount > 0) {
+            array_push($intervalstr, mb_substr($inputdata, $trialcount, 2));
+            $trialcount = $trialcount + 1;
+            $intervalcount = $intervalcount - 1;
+        }
+
+        return $this->intervalstr = $intervalstr;
+    }
+
+    public function IntervalInt()
+    {
+        $intervalint = [
+            "AB"=>array(1090, 60),
+            "BA"=>array(1090, 60),
+            "AD"=>array(540, 50),
+            "DA"=>array(540, 50),
+            "AC"=>array(180, 60),
+            "CA"=>array(180, 60),
+            "BC"=>array(960, 60),
+            "CB"=>array(960, 60),
+            "BG"=>array(1270, 60),
+            "GB"=>array(1270, 60),
+            "CD"=>array(400, 50),
+            "DC"=>array(400, 50),
+            "CF"=>array(200, 60),
+            "FC"=>array(200, 60),
+            "DF"=>array(510, 50),
+            "FD"=>array(510, 50),
+            "FG"=>array(230, 50),
+            "GF"=>array(230, 50),
+            "DE"=>array(720, 50),
+            "ED"=>array(720, 50),
+            "EG"=>array(1050, 50),
+            "GE"=>array(1050, 50)
+            ];
+
+        return $this->intervalint = $intervalint;
+    }
+
+    public function PriceCalc()
+    {
+        $startdata = $this->startdata;
+        $intervalstr = $this->intervalstr;
+        $intervalint = $this->intervalint;
+
+        $upmeter = $startdata[0];
+        $meter = 0;
+        $price = array($startdata[1]);
+
+        foreach ($intervalstr as $key=>$interval) {
+            $meter = $meter + $intervalint[$interval][0];
+
+            if ($meter > $upmeter) {
+                while ($meter > $upmeter) {
+                    $upmeter = $upmeter + 200;
+                    array_push($price, $intervalint[$interval][1]);
+                }
+            }
+        }
+
+        $pricecalc = array_sum($price);
+        return $this->pricecalc = $pricecalc;
+    }
 }
--- tests/taxiTest.php
+++ tests/taxiTest.php
@@ -26,4 +26,64 @@ class taxiTest extends \PHPUnit_Framework_TestCase
         $this->setExpectedException('\TripleI\taxi\Exception\LogicException');
         throw new Exception\LogicException;
     }
+
+    public function testPrice()
+    {
+        $testdata = array(
+                        array( "ADFC", "510" ),
+                        array( "CFDA", "500" ), 
+                        array( "AB", "460" ),
+                        array( "BA", "460" ),
+                        array( "CD", "400" ),
+                        array( "DC", "350" ),
+                        array( "BG", "520" ),
+                        array( "GB", "530" ),
+                        array( "FDA", "450" ),
+                        array( "ADF", "450" ),
+                        array( "FDACB", "750" ),
+                        array( "BCADF", "710" ),
+                        array( "EDACB", "800" ),
+                        array( "BCADE", "810" ),
+                        array( "EGFCADE", "920" ),
+                        array( "EDACFGE", "910" ),
+                        array( "ABCDA", "960" ),
+                        array( "ADCBA", "1000" ),
+                        array( "BADCFGB", "1180" ),
+                        array( "BGFCDAB", "1180" ),
+                        array( "CDFC", "460" ),
+                        array( "CFDC", "450" ),
+                        array( "ABGEDA", "1420" ),
+                        array( "ADEGBA", "1470" ),
+                        array( "CFGB", "640" ),
+                        array( "BGFC", "630" ),
+                        array( "ABGEDFC", "1480" ),
+                        array( "CFDEGBA", "1520" ),
+                        array( "CDFGEDABG", "1770" ),
+                        array( "GBADEGFDC", "1680" )
+                );
+
+        $taxi = new taxi;
+        foreach ($testdata as $key=>$test) {
+
+            $taxi->inputdata;
+            $taxi->startdata;
+            $taxi->intervalstr;
+            $taxi->intervalint;
+            $taxi->pricecalc;
+    
+            $taxi->InputData($test[0]);
+            $taxi->StartData();
+            $taxi->IntervalStr();
+            $taxi->IntervalInt();
+            $answer = $taxi->Pricecalc();
+
+            $this->assertEquals($answer, $test[1]);
+    
+            if ($test[1] = $answer) {
+                echo $test[1], " = ", $answer, "\n";
+            } else {
+                echo $test[1], " = ", $answer, "エラー\n";
+            }
+        }
+    }
 }
